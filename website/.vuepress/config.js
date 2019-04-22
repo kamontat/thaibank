ef6edbab6fad5441190883ba5d398fa3614c05a5
Kamontat Chantrachirathumrong <kamontat.c@hotmail.com>
2019-04-23 00:49:42 +0700

    feat(website): add i18n, more page more nav and sidebar

--- website/.vuepress/config.js
+++ website/.vuepress/config.js
@@ -9,14 +9,39 @@ module.exports = {
       content: package.author
     }]
   ],
+  locales: {
+    '/': {
+      lang: 'th-TH',
+      title: 'ธนาคารในประเทศไทย',
+      description: package.descriptionTH,
+    },
+    '/en/': {
+      lang: 'en-US',
+      title: 'Thai bank',
+      description: package.description,
+    }
+  },
   dest: 'public',
   plugins: {
-    '@vuepress/last-updated': {},
+    '@vuepress/last-updated': {
+      transformer: (timestamp, lang) => {
+        // Don't forget to install moment yourself
+        const moment = require('moment')
+        moment.locale(lang)
+        return moment(timestamp).format("DD MMM YYYY HH:mm:ss")
+      }
+    },
     '@vuepress/medium-zoom': {},
-    // '@vuepress/plugin-active-header-links': {},
-    // '@vuepress/plugin-back-to-top': {},
-    // '@vuepress/plugin-google-analytics': {},
-    // 'vuepress-plugin-flowchart': {},
+    '@vuepress/plugin-active-header-links': {
+      sidebarLinkSelector: '.sidebar-link',
+      headerAnchorSelector: '.header-anchor',
+      headerTopOffset: 120
+    },
+    '@vuepress/plugin-back-to-top': {},
+    '@vuepress/plugin-google-analytics': {
+      'ga': 'UA-124896160-11'
+    },
+    'flowchart': {},
     // '@vssue/vuepress-plugin-vssue': {
     //   // set `platform` rather than `api`
     //   platform: 'github',
@@ -45,32 +70,12 @@ module.exports = {
     }
   },
   themeConfig: {
+    displayAllHeaders: true,
+    activeHeaderLinks: true,
     locales: {
       '/': {
-        lang: 'en-US', // this will be set as the lang attribute on <html>
-        title: 'Thai bank',
-        description: package.descriptionTH,
-        // text for the language dropdown
-        selectText: 'Languages',
-        // label for this locale in the language dropdown
-        label: 'English',
-        // Customising the header label
-        // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
-        repoLabel: 'Contribute!',
-        // custom text for edit link. Defaults to "Edit this page"
-        editLinkText: 'Help us improve this page!',
-        // config for Service Worker 
-        serviceWorker: {
-          updatePopup: {
-            message: "New content is available.",
-            buttonText: "Refresh"
-          }
-        },
-      },
-      '/th/': {
+        sidebar: 'auto',
         lang: 'th-TH',
-        title: 'ธนาคารในประเทศไทย',
-        description: package.description,
         // text for the language dropdown
         selectText: 'ภาษา',
         // label for this locale in the language dropdown
@@ -80,6 +85,7 @@ module.exports = {
         repoLabel: 'สนับสนุน !',
         // custom text for edit link. Defaults to "Edit this page"
         editLinkText: 'ช่วยเราทำให้หน้านี้ดีขึ้น!',
+        lastUpdated: 'อัพเดตล่าสุด',
         // config for Service Worker 
         serviceWorker: {
           updatePopup: {
@@ -87,7 +93,131 @@ module.exports = {
             buttonText: "อัพเดต"
           }
         },
-      }
+        nav: [{
+          text: 'เปรียบเทียบ',
+          items: [{
+            text: 'บัญชี',
+            link: '/compare/account'
+          }, {
+            text: 'บัตรเครดิต',
+            link: '/compare/credit'
+          }, {
+            text: 'แอพพลิเคชั่น',
+            link: '/compare/application'
+          }, {
+            text: 'เว็บไซต์',
+            link: '/compare/website'
+          }]
+        }, {
+          text: 'ธนาคาร',
+          items: [{
+            text: 'ธนาคารกสิกรไทย',
+            link: '/bank/k'
+          }, {
+            text: 'ธนาคารไทยพาณิชย์',
+            link: '/bank/scb'
+          }, {
+            text: 'ธนาคารกรุงศรีอยุธยา',
+            link: '/bank/ks'
+          }, {
+            text: 'ธนาคารกรุงไทย',
+            link: '/bank/ktb'
+          }, {
+            text: 'ธนาคารกรุงเทพ',
+            link: '/bank/bb'
+          }, {
+            text: 'ธนาคารทหารไทย',
+            link: '/bank/tmb'
+          }, {
+            text: 'ธนาคารธนชาต',
+            link: '/bank/t'
+          }, {
+            text: 'ธนาคารออมสิน',
+            link: '/bank/gsb'
+          }]
+        }, {
+          text: 'ข้อมูล',
+          items: [{
+            text: 'ลิขสิทธิ์',
+            link: '/info/licenses'
+          }, {
+            text: 'ผู้สนับสนุน',
+            link: '/info/contribute'
+          }]
+        }],
+      },
+      '/en/': {
+        lang: 'en-US', // this will be set as the lang attribute on <html>
+        // text for the language dropdown
+        selectText: 'Languages',
+        // label for this locale in the language dropdown
+        label: 'English',
+        // Customising the header label
+        // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
+        repoLabel: 'Contribute!',
+        // custom text for edit link. Defaults to "Edit this page"
+        editLinkText: 'Help us improve this page!',
+        lastUpdated: 'Last Updated',
+        // config for Service Worker 
+        serviceWorker: {
+          updatePopup: {
+            message: "New content is available.",
+            buttonText: "Refresh"
+          }
+        },
+        nav: [{
+          text: 'comparison',
+          items: [{
+            text: 'Account',
+            link: '/compare/account'
+          }, {
+            text: 'Credit card',
+            link: '/compare/credit'
+          }, {
+            text: 'Application',
+            link: '/compare/application'
+          }, {
+            text: 'Website',
+            link: '/compare/website'
+          }]
+        }, {
+          text: 'Bank',
+          items: [{
+            text: 'Kasikorn Bank',
+            link: '/bank/k'
+          }, {
+            text: 'Siam Commercial Bank',
+            link: '/bank/scb'
+          }, {
+            text: 'Bank of Ayudhya',
+            link: '/bank/ks'
+          }, {
+            text: 'Krungthai Bank',
+            link: '/bank/ktb'
+          }, {
+            text: 'Bangkok Bank',
+            link: '/bank/bb'
+          }, {
+            text: 'TMB Bank',
+            link: '/bank/tmb'
+          }, {
+            text: 'Thanachart Bank',
+            link: '/bank/t'
+          }, {
+            text: 'Government Savings Bank',
+            link: '/bank/gsb'
+          }]
+        }, {
+          text: 'Information',
+          items: [{
+            text: 'License',
+            link: '/info/licenses'
+          }, {
+            text: 'Contribute',
+            link: '/info/contribute'
+          }]
+        }]
+      },
     },
     // Assumes GitHub. Can also be a full GitLab url.
     repo: 'kamontat/thaibank',
--- website/README.md
+++ website/README.md
@@ -1,1 +1,29 @@
-Hello world
+---
+home: true
+heroImage: /features/images/hero.jpeg
+heroText: ข้อมูลของธนาคารในประเทศไทย
+tagline: เปรียบเทียบ . ธนาคาร . ไทย
+actionText: เริ่มเลย →
+actionLink: /welcome/
+features:
+- title: ข้อมูลทั่วไปของธนาคาร
+  details: ช่วยลดเวลาในการหาข้อมูลในที่ต่างๆ และยังไม่รู้อีกว่ามันอัพเดตล่าสุดไหม.
+- title: เปรียบเทียบการใช้การบริการของธนาคาร
+  details: เคยไหม ที่ไม่รู้ว่าจะเปิดบัญชีธนาคารอะไรดี ไม่รู้ว่าแต่ละธนาคารมีจุดเด่น จุดด้อยยังไง
+- title: ง่ายยังไม่พอ เร็วอีกตังหาก
+  details: เนื่องจากเว็บนี้สร้างจากเทคโนโลยีที่ค่อนข้างจะสมัยใหม่ จึงทำให้แน่ใจได้ว่าจะสามารถเปิดได้ไวกว่าเว็บไซต์ของหลายๆ ธนาคาร
+---
+
+@flowstart vue
+
+st=>start: Open
+search=>operation: Search / Click
+e=>end: Receive
+
+st->search->e
+
+@flowend
+
+> Credit: [banner image](https://medium.com/@pathummasutmark/%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%81%E0%B9%88%E0%B8%AD%E0%B8%95%E0%B8%B1%E0%B9%88%E0%B8%87%E0%B8%98%E0%B8%99%E0%B8%B2%E0%B8%84%E0%B8%B2%E0%B8%A3%E0%B8%95%E0%B9%88%E0%B8%B2%E0%B8%87%E0%B9%86%E0%B9%86-bb810f4565f1)
+>
+> <p>ผลงานนี้ ใช้<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">สัญญาอนุญาตของครีเอทีฟคอมมอนส์แบบ แสดงที่มา-ไม่ใช้เพื่อการค้า-อนุญาตแบบเดียวกัน 4.0 International</a>.</p>
