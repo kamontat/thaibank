7f79103a33c6d33653519ef1580b7cb2d4d2e341
Kamontat Chantrachirathumrong <kamontat.c@hotmail.com>
2019-04-22 15:33:13 +0700

    impr(helper): add deploy script

---
+++ deploy.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1000
+
+# generate by create-script-file v4.0.0
+# link (https://github.com/Template-generator/create-script-file/tree/v4.0.0)
+
+# set -x #DEBUG - Display commands and their arguments as they are executed.
+# set -v #VERBOSE - Display shell input lines as they are read.
+# set -n #EVALUATE - Check syntax of the script but don't execute.
+
+#/ -----------------------------------
+#/ Description:  ...
+#/ How to:       ...
+#/               ...
+#/ Option:       --help | -h | -? | help | h | ?
+#/                   > show this message
+#/               --version | -v | version | v
+#/                   > show command version
+#/ -----------------------------------
+#/ Create by:    Kamontat Chantrachirathunrong <kamontat.c@hotmail.com>
+#/ Since:        22/04/2019
+#/ -----------------------------------
+#/ Error code    1      -- error
+#/ -----------------------------------
+#/ Known bug:    ...
+#/ -----------------------------------
+#// Version:      0.0.1   -- description
+#//               0.0.2b1 -- beta-format
+#//               0.0.2a1 -- alpha-format
+
+# abort on errors
+set -e
+
+USERNAME="kamontat"
+REPO="thaibank"
+DEST_FOLDER="public"
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd "$DEST_FOLDER" || exit 1
+
+# if you are deploying to a custom domain
+echo 'bank.kamontat.net' >CNAME
+
+git add -A
+git commit -m 'chore(release): update new version'
+
+git push "git@github.com:$USERNAME/$REPO.git" master:gh-pages
+
+cd - || exit 2
