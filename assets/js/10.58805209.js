e145e1c04f1b14c1334a049bb9f99d2b7946e6c4
cibot <ci@kamontat.net>
2019-04-23 10:45:10 +0000

    chore(release): push new release to github

---
+++ assets/js/10.58805209.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{282:function(t,n,e){"use strict";e.r(n);var s=e(1),l=Object(s.a)({},function(){var t=this.$createElement,n=this._self._c||t;return n("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[n("p",[this._v("ข้อมูลทั่วไปของธนาคาร (KBank) รวมถึง เบอร์โทรต่างๆ")])])},[],!1,null,null,null);n.default=l.exports}}]);
